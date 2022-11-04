d0fabcd8767706d3e1cc004a9c2990595c8eafb6
alyssa-adams <amadam15@asu.edu>
2022-11-04 20:30:00 +0900

    Tutorial circuit

--- circuits.py
+++ circuits.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import PySpice.Logging.Logging as Logging
+from PySpice.Spice.Netlist import Circuit
+from PySpice.Unit import *
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    logger = Logging.setup_logging()
+
+    circuit = Circuit('Voltage Divider')
+    circuit.V('input', 'in', circuit.gnd, 10 @ u_V)
+    circuit.R(1, 'in', 'out', 9 @ u_kΩ)
+    circuit.R(2, 'out', circuit.gnd, 1 @ u_kΩ)
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+
+    analysis = simulator.operating_point()
+    analysis
+    for node in (analysis['in'], analysis.out):  # .in is invalid !
+        print('Node {}: {} V'.format(str(node), float(node)))
